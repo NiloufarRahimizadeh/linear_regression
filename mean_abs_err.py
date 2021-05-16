a81a98ee1c9359394f5cb2e431f07d3030a7c562
poulstar <info@poulstar.com>
2021-05-16 16:48:34 +0430

    third commit

--- mean_abs_err.py
+++ mean_abs_err.py
@@ -1,5 +1,8 @@
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+from numpy import *
 
 y_true = [3, -0.5, 2, 7]
 y_pred = [2.5, 0.0, 2, 8]
@@ -8,3 +11,6 @@ print(e1)
 
 e2 = mean_squared_error(y_true, y_pred)
 print(e2)
+
+e3 =sqrt(e2)
+print(e3)
